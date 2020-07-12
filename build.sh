e0124c3dbc7a4d68f0d9caae4ce26027bea2afaf
keithconger <keith.conger@blackboxembedded.com>
2020-07-12 12:35:29 -0600

    NEW: Localized images NEW: Build Script NEW: Structure to host in GitHub Pages

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Generate translated documentation
+#
+
+for LANGS in `ls -1 po`
+do
+	LANG=$(echo $LANGS | cut -d'.' -f1)
+	for TEMPLATES in `ls -1 templates`
+	do
+		if [ -d "docs/$LANG" ] 
+		then
+    			echo "Directory exists: $LANG"
+		else
+			echo "Creating a directory: $LANG"
+			mkdir docs/$LANG
+		fi
+
+		# Translate templates
+		po2html --threshold=100 po/$LANGS -t templates/$TEMPLATES docs/$LANG/$TEMPLATES
+		
+		# Copy base localized images
+		cp -rpf images-localized/en docs/$LANG/images-localized/
+		
+		# Copy any localized images for the language
+		cp -rpf images-localized/$LANG/* docs/$LANG/images-localized/
+	done
+done
